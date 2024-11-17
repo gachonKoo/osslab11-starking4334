bfe5a085ad83eccdb9a604cb0503417776a7cfc2
starking4334 <66519799+starking4334@users.noreply.github.com>
2024-11-17 17:19:55 +0900

    Delete geo directory

--- geo/utils.py
+++
@@ -1,9 +0,0 @@
-import math
-
-def hypotenuse(a, b):
-    """Calculate the hypotenuse of a right triangle."""
-    return math.sqrt(a**2 + b**2)
-
-def circle_area(r):
-    """Calculate the area of a circle."""
-    return math.pi * r**2
